49f7fc5d6f56f21fb96d64e3b0efd555402b2779
yoonsun <krjeongys@gmail.com>
2019-11-14 15:42:41 +0900

    hidden inner slide objects, change button color to white

--- YSTestTheme/src/core-components/AnimatedSlider.js
+++ YSTestTheme/src/core-components/AnimatedSlider.js
@@ -10,21 +10,21 @@ const AnimatedSlider = () => {
             description:
                 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
             button: 'Read More',
-            image: require('../assets/images/fujixerox1.jpg')
+            image: require('../assets/images/banner1.jpg')
         },
         {
             title: 'Tortor Dapibus Commodo Aenean Quam',
             description:
                 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
             button: 'Discover',
-            image: require('../assets/images/fujixerox2.jpg')
+            image: require('../assets/images/banner2.jpg')
         },
         {
             title: 'Phasellus volutpat metus',
             description:
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
             button: 'Buy now',
-            image: require('../assets/images/fujixerox3.jpg')
+            image: require('../assets/images/banner3.jpg')
         }
     ];
 
@@ -37,7 +37,7 @@ const AnimatedSlider = () => {
                             className="slider-content"
                             style={{ background: `url('${item.image}') no-repeat center center` }}
                         >
-                            <div className="inner">
+                            <div className="inner" visibility="hidden">
                                 <h1>{item.title}</h1>
                                 <p>{item.description}</p>
                                 <button>{item.button}</button>
